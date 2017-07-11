57e0398bbb75c927b969924a22009b5c2321fa4d
= <=>
2017-07-11 13:23:47 +0530

    Angular Binding Assingment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title:string;
+  firstname:string;
+  lastname:string;
+
+  ngOnInit() {
+  
+   // variable declaration 
+       this.firstname="Firstname"
+       this.lastname="Lastname"
+        this.title = this.firstname+" "+this.lastname;
+ 
+     }
+
+     clickme() {
+
+       this.title = this.firstname+" "+this.lastname;
+     }
+
+
 }
